025ff43983abbda98ebbb3715bf25398b3743e44
Hunor <hunorgaming000@gmail.com>
2024-09-05 20:03:59 +0200

    bug fixes, guide and commenting out prints, ton of testing and changing

--- Server_Finder.py
+++ Server_Finder.py
@@ -4,6 +4,9 @@ import psutil
 from scapy.all import sniff, UDP, IP
 import time
 import sys
+import ctypes
+
+ctypes.windll.kernel32.SetConsoleTitleW("Sea of Thieves Server Finder")
 
 sotID = ""
 sotPorts = []
@@ -14,12 +17,10 @@ def initialize():
     global sotID
     print("Welcome to Sea of Thieves Server Finder!\nGuide:")
     print(" 1.) You and your friend have to start the game and join a server.")
-    print(" 2.) Type \"y\" and search for ip adress.")
-    print(" 3.) Check if you have matching ip adresses with your friend.")
-    print(" 4.) If you don't, server hop to another server in game, and search for your ip again.")
-    print("     If you have matching ip adresses, you're good to go!\n")
-    start = input("Do you want to start searching? Y / N or any other key (exit)\n")
-    if start == "Y" or start == "y":
+    print(" 2.) Press Enter to search for the server's ip adress.")
+    print(" 3.) Server hop until your ip adresses and ports are matching.")
+    start = input("Do you want to start searching? Press Enter / Press any other key to exit\n")
+    if start == "":
         print()
         sotPorts.clear()
         captured_ip.clear()
@@ -31,10 +32,11 @@ def initialize():
             clear = lambda: os.system('cls')
             clear()
             initialize()
-        getSoTPort()
-        sniffFoundPorts()
-        restart = input("    Do you want to try again? Y / N\n")
-        if restart == "Y" or restart == "y":
+        else:
+            getSoTPort()
+            sniffingSotIp()
+        restart = input("   Do you want to try again?\n      Press Enter / Type any other key to exit\n")
+        if restart == "":
             restarted()
         else:
             clear = lambda: os.system('cls')
@@ -44,8 +46,8 @@ def initialize():
         sys.exit()
 
 def restarted():
+    print()
     global sotID
-    print("\n")
     sotPorts.clear()
     captured_ip.clear()
     captured_port.clear()
@@ -56,28 +58,21 @@ def restarted():
         clear = lambda: os.system('cls')
         clear()
         initialize()
-    getSoTPort()
-    sniffFoundPorts()
-    restart = input("    Do you want to try again? Y / N\n")
-    if restart == "Y" or restart == "y":
-            restarted()
     else:
-        clear = lambda: os.system('cls')
-        clear()
-        initialize()
-
+        getSoTPort()
+        sniffingSotIp()
+    
 def findSotID():
-    print(f"Searching for Sea of Thieves PID...", end=" ")
+    #print(f"Searching for Sea of Thieves PID...", end=" ")
     processes = psutil.process_iter()
     for process in processes:
         if (process.name() == "SoTGame.exe"):
             sotID = str(process.pid)
-            print(f"Found! ({sotID})")
+            #print(f"Found! ({sotID})")
             return sotID
         
 def getSoTPort():
-    print(f"Searching for local Sea of Thieves Port...", end=" ")
-    sotPorts.clear()
+    #print(f"Searching for local Sea of Thieves Port...", end=" ")
     try:  
         activeConnections = subprocess.run("netstat -anop udp", stdout=subprocess.PIPE).stdout.decode('utf-8')
         activeConnections.strip()
@@ -85,9 +80,9 @@ def getSoTPort():
         for i in connectionslist:
             if sotID in i:
                 sotPorts.append(int(i.split(":")[1].split(" ")[0]))
-        print(f"Found! ({sotPorts[1]})")
+        sotPorts[1] #To throw an error
     except:
-        print("Ports cannot be found! Aborting... (Join a server)")
+        print("Local ports cannot be found! Aborting... (Join a server)")
         time.sleep(5)
         clear = lambda: os.system('cls')
         clear()
@@ -102,9 +97,25 @@ def process_packet(packet):
             captured_ip.append(server_ip)
             captured_port.append(server_port)
         
-def sniffFoundPorts():
-    print("Sniffing for Sea of Thieves Server IP...")
-    sniff(filter=f"udp port {sotPorts[1]}", prn=process_packet, count=10, timeout=20)
-    print(f"\nFound Sea of thieves server ip as: {captured_ip[0]}:{captured_port[0]}")
+def sniffingSotIp():
+    #print("Sniffing for Sea of Thieves Server IP...")
+    try:
+        sniff(filter=f"udp port {sotPorts[1]}", prn=process_packet, count=10, timeout=5)
+        print(f"Found Sea of thieves server ip as: {captured_ip[0]}:{captured_port[0]}")
+        
+        restart = input("    Do you want to try again?\n      Press Enter / Press any other key to exit\n")
+        if restart == "":
+            restarted()
+        else:
+            clear = lambda: os.system('cls')
+            clear()
+            initialize()
+    except:
+        print("Server cannot be found! You probably left the game! Aborting...")
+        time.sleep(5)
+        clear = lambda: os.system('cls')
+        clear()
+        initialize()
+        
     
 initialize()
